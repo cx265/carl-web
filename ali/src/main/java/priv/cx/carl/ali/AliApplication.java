33a4e17b6dff4465fb80c5400f5ab485df853bb5
cx265 <cx265@vip.qq.com>
2018-12-30 16:07:23 +0800

    build project

---
+++ ali/src/main/java/priv/cx/carl/ali/AliApplication.java
@@ -0,0 +1,14 @@
+package priv.cx.carl.ali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliApplication.class, args);
+    }
+
+}
+
