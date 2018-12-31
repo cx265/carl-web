7266821d677b62d4e84eea1a2d02476907a96170
cx265 <cx265@vip.qq.com>
2018-12-31 20:09:43 +0800

    eureka server

--- ali/eureka/src/test/java/priv/cx/carl/eureka/EurekaApplicationTests.java
+++ ali/eureka/src/test/java/priv/cx/carl/eureka/EurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package priv.cx.carl.ali;
+package priv.cx.carl.eureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AliApplicationTests {
+public class EurekaApplicationTests {
 
     @Test
     public void contextLoads() {
