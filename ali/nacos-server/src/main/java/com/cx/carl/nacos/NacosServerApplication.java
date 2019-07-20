fce924ea49466ff66de7768c6c3a4d791fe54f82
cx265 <cx265@vip.qq.com>
2019-07-20 09:59:07 +0800

    build project

---
+++ ali/nacos-server/src/main/java/com/cx/carl/nacos/NacosServerApplication.java
@@ -0,0 +1,20 @@
+package com.cx.carl.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenxuan
+ * @version : 1.0.0
+ * @description : nacos server
+ * @email : chenx265@163.com
+ * @date : 2018/12/31 15:36
+ */
+@SpringBootApplication
+public class NacosServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosServerApplication.class, args);
+    }
+
+}
